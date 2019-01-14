9a982b7383dae4bbf3f8e856118801a8f838c4b4
jouraravi1 <43246410+jouraravi1@users.noreply.github.com>
2019-01-14 16:58:41 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,17 @@
+
+//Name:        Ravi Joura
+//Date:        January 14, 2019
+//Student_Id:  C0745121
+
+alert("yeah i am great.......")
+let countDown;
+let timeInterval;
+const endDate = document.querySelector('input[name="endDate"]');
+const clock = document.querySelector('#clock');
+const daysSpan = clock.querySelector('.days');
+const HoursSpan = clock.querySelector('.hours');
+const MinuteSpan = clock.querySelector('.Minute');
+const SecondsSpan = clock.querySelector('.Seconds');
+
+console.log("jdjdfd");
+console.log(endDate.value);
